cd08373d44b43d1d04201a13b587b98c49cbaa4b
MihalisP <mihalispapak@gmail.com>
2022-03-13 14:53:04 +0200

    add wordcloud generation method

---
+++ utils/analyzer.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from nltk.corpus import stopwords
+
+import settings
+from wordcloud import WordCloud
+
+
+def generate_wordcloud(df: pd.DataFrame, column: str) -> str:
+    filename = f'{settings.IMG_DIR}{column}_wordcloud.png'
+
+    text = " ".join([val for val in df[column].values])
+    wordcloud = WordCloud(
+        width=800,
+        height=800,
+        stopwords=stopwords.words('english'),
+        background_color='black',
+        min_font_size=10).generate(text)
+    wordcloud.to_file(filename)
+    return f'/{filename}'
